eac708f931e952e24f650df6e1da11ee86137040
shenils <shenil@bu.edu>
2015-09-01 12:10:01 +0530

    decimal corner cases

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -18,21 +18,56 @@ var calc = {
       $('#preview').html('');
       $('#result').html('');
     });
-    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(function(digit) {
-    $(document).bind('keyup', digit, function() {
-      handleInput(digit);
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '+', '-', '.'].forEach(function(digit) {
+      $(document).bind('keyup', digit, function() {
+        calc.handleInput(digit);
+      });
     });
-  });
-    
+    $(document).bind('keyup', '.', function() {
+      lastNumber = calc.getLastNumber();
+      if(lastNumber.indexOF('.') == -1) {
+        if(lastNumber.length == 0) {
+          calc.handleInput(0);
+        }
+        calc.handleInput('.');
+      }
+    });
+    $(document).bind('keyup', 'backspace', function() {
+      calc.handleDelete();
+    });
+    $(document).bind('keyup', 'shift+=', function() {
+      calc.handleInput('+');
+    }); 
+    ['=', 'return'].forEach(function(key) {
+      $(document).bind('keyup', key, function() {
+        calc.evaluateResult();
+      });
+    });  
   },
   handleInput(input): function() {
     $('#preview').html($('#preview').html() + input);
   },
   handleDelete(): function() {
     $('#preview').html($('#preview').html().slice(0, -1));
+    if($('#preview').html().length == 1) {
+      calc.clearResult();
+    }
   },
   evaluateResult(): function() {
     $('#result').html(eval($('#preview').html()));
+  },
+  clearResult(): function() {
+    $('#result').html('');
+  },
+  getLastNumber() function {
+    str = $('#preview').html();
+    regexp = /[+\-*\/]([0-9.])*$/
+    matches = str.match(regexp);
+    if (matches == null) {
+      return str;
+    } else {
+      return matches[0].slice(1);
+    }
   }   
 }
 
